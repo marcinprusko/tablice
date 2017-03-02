63217acef0001e2005ba1fde11fb7eee593a9b56
marcinprusko <marcinprusko@gmail.com>
2017-03-02 22:03:17 +0100

    tablice

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+﻿// plik scripts.js
+
+var names1 = ['Aga', 'Kasia', 'Małgosia', 'Ala'];
+var names2 = ['Janek', 'Paweł', 'Andrzej', 'Krzyś'];
+var allNames = names1.concat(names2);
+
+console.log(allNames);
+
+var newName = 'Mariusz';
+if (allNames.indexOf(newName) === -1) {
+	console.log(allNames.push('Mariusz'))
+} 
